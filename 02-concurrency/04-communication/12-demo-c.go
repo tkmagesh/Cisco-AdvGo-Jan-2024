a4e548232f8fe5372d97ee635c98b3dd06248cf8
Magesh Kuppan <tkmagesh77@gmail.com>
2024-01-10 16:06:10 +0530

    send-only & receive-only channels

---
+++ 02-concurrency/04-communication/12-demo-c.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// consumer
+func main() {
+
+	ch := add(100, 200)
+	result := <-ch
+	fmt.Println(result)
+}
+
+// producer
+func add(x, y int) <-chan int {
+	// share memory by communicating
+	ch := make(chan int)
+	go func() {
+		time.Sleep(2 * time.Second)
+		result := x + y
+		ch <- result
+	}()
+	return ch
+}
