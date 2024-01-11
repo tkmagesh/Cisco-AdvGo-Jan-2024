be7409e732651ecd29c8e3cb982d8be14186fab0
Magesh Kuppan <tkmagesh77@gmail.com>
2024-01-11 16:27:18 +0530

    Bidirectional Streaming

--- 06-grpc-app/client/client.go
+++ 06-grpc-app/client/client.go
@@ -29,7 +29,8 @@ func main() {
 	// doRequestResponse(ctx, appServiceClient)
 	// doServerStreaming(ctx, appServiceClient)
 	// doServerStreamingWithCancellation(ctx, appServiceClient)
-	doClientStreaming(ctx, appServiceClient)
+	// doClientStreaming(ctx, appServiceClient)
+	doBiDiStreaming(ctx, appServiceClient)
 }
 
 func doClientStreaming(ctx context.Context, appServiceClient proto.AppServiceClient) {
@@ -53,6 +54,49 @@ func doClientStreaming(ctx context.Context, appServiceClient proto.AppServiceCli
 	} else {
 		log.Fatalln(err)
 	}
+}
+
+func doBiDiStreaming(ctx context.Context, appServiceClient proto.AppServiceClient) {
+	persons := []*proto.PersonName{
+		{FirstName: "Magesh", LastName: "Kuppan"},
+		{FirstName: "Suresh", LastName: "Kannan"},
+		{FirstName: "Ramesh", LastName: "Jayaraman"},
+		{FirstName: "Rajesh", LastName: "Pandit"},
+		{FirstName: "Ganesh", LastName: "Kumar"},
+	}
+
+	clientStream, err := appServiceClient.Greet(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	done := make(chan struct{})
+	go func() {
+		for {
+			res, err := clientStream.Recv()
+			if err != nil {
+				close(done)
+				log.Println(err)
+				break
+			}
+			log.Println(res.GetMessage())
+		}
+
+	}()
+
+	for _, person := range persons {
+		req := &proto.GreetRequest{
+			Person: person,
+		}
+		log.Printf("Sending Person : %s %s\n", person.FirstName, person.LastName)
+		if err := clientStream.Send(req); err != nil {
+			log.Fatalln(err)
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+	clientStream.CloseSend()
+
+	<-done
 }
 
 func doServerStreaming(ctx context.Context, appServiceClient proto.AppServiceClient) {
--- 06-grpc-app/server/server.go
+++ 06-grpc-app/server/server.go
@@ -88,6 +88,39 @@ LOOP:
 	return nil
 }
 
+func (asi *AppServiceImpl) Greet(serverStream proto.AppService_GreetServer) error {
+	for {
+		greetReq, err := serverStream.Recv()
+		if code := status.Code(err); code == codes.Unavailable {
+			fmt.Println("Client connection closed")
+			break
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		person := greetReq.GetPerson()
+		firstName := person.GetFirstName()
+		lastName := person.GetLastName()
+		log.Printf("Received greet request for %q and %q\n", firstName, lastName)
+		message := fmt.Sprintf("Hi %s %s, Have a nice day!", firstName, lastName)
+		time.Sleep(2 * time.Second)
+		log.Printf("Sending response : %q\n", message)
+		greetResp := &proto.GreetResponse{
+			Message: message,
+		}
+		if err := serverStream.Send(greetResp); err != nil {
+			if code := status.Code(err); code == codes.Unavailable {
+				fmt.Println("Client connection closed")
+				break
+			}
+		}
+	}
+	return nil
+}
+
 func main() {
 	asi := &AppServiceImpl{}
 	listener, err := net.Listen("tcp", ":50051")
