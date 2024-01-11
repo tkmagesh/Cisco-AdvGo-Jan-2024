5f0620a76dfc82eee3cc497e60fac6e716b5833f
Magesh Kuppan <tkmagesh77@gmail.com>
2024-01-11 14:14:24 +0530

    Request & Response implementation

--- 06-grpc-app/client/client.go
+++ 06-grpc-app/client/client.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"grpc-app/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
 
 func main() {
-	fmt.Println("client executed")
+	options := grpc.WithTransportCredentials(insecure.NewCredentials())
+	clientConn, err := grpc.Dial("localhost:50051", options)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	appServiceClient := proto.NewAppServiceClient(clientConn)
+	ctx := context.Background()
+	req := &proto.AddRequest{
+		X: 100,
+		Y: 200,
+	}
+	addResponse, err := appServiceClient.Add(ctx, req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Add Result :", addResponse.GetResult())
 }
--- 06-grpc-app/server/server.go
+++ 06-grpc-app/server/server.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"grpc-app/proto"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type AppServiceImpl struct {
+	proto.UnimplementedAppServiceServer
+}
+
+func (as *AppServiceImpl) Add(ctx context.Context, req *proto.AddRequest) (*proto.AddResponse, error) {
+	x := req.GetX()
+	y := req.GetY()
+	fmt.Printf("Processing Add Request with x = %d and y = %d\n", x, y)
+	result := x + y
+	fmt.Printf("Responding with result : %d\n", result)
+	res := &proto.AddResponse{
+		Result: result,
+	}
+	return res, nil
+}
 
 func main() {
-	fmt.Println("server executed")
+	asi := &AppServiceImpl{}
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	grpcServer := grpc.NewServer()
+	proto.RegisterAppServiceServer(grpcServer, asi)
+	grpcServer.Serve(listener)
 }
