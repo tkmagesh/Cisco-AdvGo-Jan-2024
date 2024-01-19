6418e796c8f100cf39812fb86c56e9a82ea08687
Magesh Kuppan <tkmagesh77@gmail.com>
2024-01-19 10:00:47 +0530

    using middlewares

--- 07-http-services/01-net-http/02-server.go
+++ 07-http-services/01-net-http/02-server.go
@@ -21,12 +21,24 @@ var products = []Product{
 	{103, "Marker", 50, 10, "Stationary"},
 }
 
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
 type MyServer struct {
-	routes map[string]func(http.ResponseWriter, *http.Request)
+	routes      map[string]http.HandlerFunc
+	middlewares []Middleware
 }
 
-func (myServer *MyServer) Register(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+func (myServer *MyServer) Register(pattern string, handler http.HandlerFunc) {
+	// myServer.routes[pattern] = handler
+	// to ensure that the handler func is executed after executing the middlewares
+	for _, middleware := range myServer.middlewares {
+		handler = middleware(handler)
+	}
 	myServer.routes[pattern] = handler
+}
+
+func (myServer *MyServer) UseMiddleware(middleware Middleware) {
+	myServer.middlewares = append(myServer.middlewares, middleware)
 }
 
 // http.Handler interface implementation
@@ -40,16 +52,18 @@ func (myServer *MyServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func NewMyServer() *MyServer {
 	return &MyServer{
-		routes: make(map[string]func(http.ResponseWriter, *http.Request)),
+		routes: make(map[string]http.HandlerFunc),
 	}
 }
 
-// handler functions
+// handler functions (type : http.HandlerFunc)
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Printf("%s - %s\n", r.Method, r.URL.Path) // refactored as a middleware
 	fmt.Fprintln(w, "Hello World!")
 }
 
 func productsHanlder(w http.ResponseWriter, r *http.Request) {
+	// fmt.Printf("%s - %s\n", r.Method, r.URL.Path) // refactored as a middleware
 	switch r.Method {
 	case http.MethodGet:
 		if err := json.NewEncoder(w).Encode(products); err != nil {
@@ -68,11 +82,22 @@ func productsHanlder(w http.ResponseWriter, r *http.Request) {
 }
 
 func customersHandler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Printf("%s - %s\n", r.Method, r.URL.Path) // refactored as a middleware
 	fmt.Fprintln(w, "All customer requests will be processed")
+}
+
+// middlewares
+func logMiddleware(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("%s - %s\n", r.Method, r.URL.Path)
+		handler(w, r)
+	}
 }
 
 func main() {
 	server := NewMyServer()
+	// using middlewares
+	server.UseMiddleware(logMiddleware)
 	server.Register("/", indexHandler)
 	server.Register("/products", productsHanlder)
 	server.Register("/customers", customersHandler)
